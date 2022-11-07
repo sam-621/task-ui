ed7e09d8c43279fbfd7d405a20e9476d5e55872f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-06 23:50:17 -0700

    update DropDownMenu

--- src/components/atoms/DropDownMenu.tsx
+++ src/components/atoms/DropDownMenu.tsx
@@ -1,10 +1,5 @@
 import { Menu, Transition } from '@headlessui/react'
-import {
-  EllipsisVerticalIcon,
-  PencilIcon,
-  PencilSquareIcon,
-  TrashIcon,
-} from '@heroicons/react/24/solid'
+import { EllipsisVerticalIcon, PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 import { Fragment } from 'react'
 
 export const DropDownMenu = () => {
@@ -22,11 +17,11 @@ export const DropDownMenu = () => {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <Menu.Items className="bg-white shadow flex flex-col gap-3 absolute rounded py-2 px-1 z-50 right-0">
+        <Menu.Items className="bg-white shadow flex flex-col gap-1 absolute rounded p-1 z-50 right-0">
           <Menu.Item>
             {({ active }) => (
               <button
-                className={`flex items-center justify-start gap-4 text-title text-sm py-2 px-3 w-32 ${
+                className={`rounded flex items-center justify-start gap-4 text-title text-sm py-2 px-3 w-32 ${
                   active && 'bg-[#F3F3F3]'
                 }`}
               >
@@ -38,12 +33,12 @@ export const DropDownMenu = () => {
           <Menu.Item>
             {({ active }) => (
               <button
-                className={`flex items-center justify-start gap-4 text-title text-sm py-2 px-3 w-32 ${
+                className={`rounded flex items-center justify-start gap-4 text-title text-sm py-2 px-3 w-32 ${
                   active && 'bg-[#F3F3F3]'
                 }`}
               >
                 <TrashIcon width={15} height={15} className="text-primary" />
-                <span>Edit</span>
+                <span>Delete</span>
               </button>
             )}
           </Menu.Item>
