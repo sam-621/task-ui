e76f4279abec94e2874ca2fad503ff086295b21f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-26 10:42:58 -0700

    add SEO

--- src/components/layout/BodyLayout.tsx
+++ src/components/layout/BodyLayout.tsx
@@ -1,7 +1,16 @@
 import { FC, ReactNode } from 'react'
+import { Helmet } from 'react-helmet'
 
 export const BodyLayout: FC<Props> = ({ children }) => {
-  return <div className=" h-screen px-5 py-4 md:px-10 lg:px-44 xl:px-96">{children}</div>
+  return (
+    <>
+      <Helmet>
+        <title>Task ui | Home</title>
+        <meta name="description" content="Aplication to manage your tasks" />
+      </Helmet>
+      <div className=" h-screen px-5 py-4 md:px-10 lg:px-44 xl:px-96">{children}</div>
+    </>
+  )
 }
 
 type Props = {
