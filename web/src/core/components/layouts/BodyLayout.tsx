14a401937b257100feb834dd0d49d11cdbd18561
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-01-01 14:20:44 -0700

    add favicon

--- web/src/core/components/layouts/BodyLayout.tsx
+++ web/src/core/components/layouts/BodyLayout.tsx
@@ -6,7 +6,7 @@ export const BodyLayout: FC<Props> = ({ children }) => {
     <>
       <Helmet>
         <title>Task ui | Home</title>
-        <meta name="description" content="Aplication to manage your tasks" />
+        <meta name="description" content="Manage your" />
       </Helmet>
       <div className="h-screen max-w-[672px] mt-4 mx-7 md:mx-auto md:mt-20">{children}</div>
     </>
