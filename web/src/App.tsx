38848053c29314496997e9e4dc20dcb188592a68
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-03-16 23:41:34 -0700

    fix(app):  remove unused lib

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Home } from './pages/Home'
 import { Toaster } from 'react-hot-toast'
 
-import 'react-toastify/dist/ReactToastify.css'
 import { BodyLayout } from '@/layouts'
 import { useUserId } from '@/hooks'
 
@@ -15,18 +14,6 @@ function App() {
       <BodyLayout>
         <Home />
       </BodyLayout>
-      {/* <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      /> */}
       <Toaster position="bottom-right" />
     </QueryClientProvider>
   )
